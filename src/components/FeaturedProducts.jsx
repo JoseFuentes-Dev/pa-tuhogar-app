140dedd5f7ce0362d0b9dcf27049c276dae48db7
unknown <thealpha9403@gmail.com>
2024-10-14 11:39:51 -0400

    agregando responsive a productos destacados

--- src/components/FeaturedProducts.jsx
+++ src/components/FeaturedProducts.jsx
@@ -4,15 +4,15 @@ import { productsdestacados } from './Productdestacados';
 
 const FeaturedProducts = () => {
   return (
-    <div className="mt-20">
+    <div className="mt-20 ">
       <h2 className="text-[1.8em] font-bold mb-6">Productos Destacados</h2>
       <div 
-        className="grid gap-6 grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6"
+        className="grid place-items-center gap-4 grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 "
       >
         {productsdestacados.map((product) => (
           <div 
             key={product.id} 
-            className="bg-white rounded-lg shadow-md p-4  "
+            className="bg-white rounded-lg shadow-md w-[330px] sm:w-[280px] md:w-full   "
             data-aos="fade-up" 
             data-aos-duration="600"
           >
@@ -23,7 +23,7 @@ const FeaturedProducts = () => {
                 className="w-full h-full object-inherit rounded-t-lg mb-2 transform transition-all duration-1000 hover:scale-110" 
               />
             </div>
-            <div className="description flex flex-col justify-between h-[170px]">
+            <div className="description flex flex-col justify-between h-[170px] p-4">
 
             <h3 className="text-[1em] font-semibold">{product.name}</h3>
             <p className="text-xl text-gray-800">${product.price.toFixed(2)}</p>
