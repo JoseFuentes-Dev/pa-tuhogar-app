d2b40a36d579af9aa83125d189868e81e855c4c6
unknown <thealpha9403@gmail.com>
2024-10-24 09:48:21 -0400

    terminado testimonials

--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -8,25 +8,40 @@ const testimonials = [
   {
     id: 1,
     name: "Juan Pérez",
-    location: "Holguín, Cuba",
+    location: "Holguín, Reparto Pedro Díaz Coello",
     comment: "¡Increíbles productos! La calidad es excepcional y el servicio al cliente es excelente.",
     rating: 5,
   },
   {
     id: 2,
     name: "María Gómez",
-    location: "La Habana, Cuba",
+    location: "Holguín, Reparto Alcides Pino",
     comment: "Me encantan mis nuevos electrodomésticos. Son muy eficientes y fáciles de usar.",
     rating: 4,
   },
   {
     id: 3,
     name: "Carlos Martínez",
-    location: "Santiago de Cuba, Cuba",
+    location: "Holguín, Reparto Lenin",
     comment: "Recomiendo esta tienda a todos mis amigos. ¡Los precios son inmejorables!",
     rating: 5,
   },
+  {
+    id: 4,
+    name: "Raquel Domínguez",
+    location: "Holguín, Reparto Pueblo Nuevo",
+    comment: "Gran variedad de productos y excelente atención al cliente.",
+    rating: 5,
+  },
+  {
+    id: 5,
+    name: "Andrés Rodríguez",
+    location: "Holguín, Reparto La Quinta",
+    comment: "Rápido, confiable y productos de muy buena calidad. ¡Lo recomiendo sin duda!",
+    rating: 4,
+  },
 ];
+
 
 const TestimonialsCarousel = () => {
   useEffect(() => {
@@ -48,15 +63,15 @@ const TestimonialsCarousel = () => {
 
   return (
     <div className="my-10 flex flex-col mt-20">
-      <h1 className="text-[1.8em] font-bold mb-4 text-center">Lo Que Dicen Nuestros Clientes</h1>
+     <h2 className="text-[2em]  font-bold mb-8 text-[#008DDA] underline decoration-[#ACE2E1]">Lo que dicen nuestros clientes</h2>
       <div className='flex justify-center my-12'>
 
       <Slider {...settings}
-       className='w-[340px] sm:w-[500px] md:w-[700px]'>
+       className='w-[340px] sm:w-[500px] md:w-[700px] '>
         {testimonials.map((testimonial) => (
           <div
             key={testimonial.id}
-            className="bg-white rounded-lg shadow-md p-8 "
+            className="bg-white rounded-lg shadow-md p-5 h-[170px]"
             data-aos="fade-up"
             >
             <p className="font-semibold text-xl">{testimonial.name}</p>
