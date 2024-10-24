2a19cb171b4d019bee74d6506061b00fa9802def
unknown <thealpha9403@gmail.com>
2024-10-24 06:58:41 -0400

    promocion de negocios perfecta

--- src/components/BusinessPromo.jsx
+++ src/components/BusinessPromo.jsx
@@ -9,23 +9,20 @@ import image3 from '../assets/images/promotion3.jpg';
 import image4 from '../assets/images/promotion4.jpg';
 import image5 from '../assets/images/promotion5.jpg';
 
-
-
 const BusinessPromo = () => {
-  const images = [image1, image2, image3, image4, image5 ];
+  const images = [image1, image2, image3, image4, image5];
 
   const settings = {
     infinite: true,
     speed: 600,
     slidesToShow: 1,
     slidesToScroll: 1,
-    arrows: false, // Opcional: Puedes mantener o eliminar las flechas
-    dots: false,   // Eliminamos los dots
-    autoplay: true, // Activa el cambio automático
-    autoplaySpeed: 3000, // Cambia la velocidad si lo necesitas
+    arrows: false,
+    dots: false,
+    autoplay: true,
+    autoplaySpeed: 3000,
   };
 
-  // Definición de las variantes de animación
   const slideVariants = {
     hidden: { opacity: 0, scale: 1.2 },
     visible: { opacity: 1, scale: 1 },
@@ -42,56 +39,58 @@ const BusinessPromo = () => {
   };
 
   const buttonVariants = {
-    hidden: { opacity: 0, x: 50 }, // Desplazado a la derecha y oculto
-    visible: { opacity: 1, x: 0 },  // Visible y en su posición original
+    hidden: { opacity: 0, x: 50 },
+    visible: { opacity: 1, x: 0 },
   };
+
   return (
-    <motion.div className="relative w-full h-[340px]   md:h-full shadow-lg rounded-[14px] overflow-hidden  "
-    initial="hidden"
-    animate="visible"
-    transition={{ duration: 1, ease: 'easeInOut' }}
-    variants={slideVariants}
+    <motion.div className="relative w-full h-[340px] md:h-full shadow-lg rounded-[14px] overflow-hidden"
+      initial="hidden"
+      animate="visible"
+      transition={{ duration: 1, ease: 'easeInOut' }}
+      variants={slideVariants}
     >
       <Slider {...settings}>
-      {images.map((image, index) => (
-            <div key={index} className="w-full h-full flex justify-center items-center ">
-              <img
-                src={image}
-                className="w-full h-[340px] sm:h-[350px] md:h-[410px]  object-initial "
-                alt={`Slide ${index + 1}`}
-              />
-            </div>
-          ))}
+        {images.map((image, index) => (
+          <div key={index} className="w-full h-full flex justify-center items-center">
+            <img
+              src={image}
+              className="w-full h-[340px] sm:h-[350px] md:h-[410px] object-cover" // Cambié a object-cover para asegurar que la imagen cubra bien el área
+              alt={`Slide ${index + 1}`}
+            />
+          </div>
+        ))}
       </Slider>
 
       {/* Texto centrado sobre las imágenes */}
-      <div className="  absolute inset-0 flex flex-col justify-center items-center bg-black bg-opacity-50 ">
-        <motion.h2 className="text-xl md:text-2xl xl:text-3xl font-bold text-white text-center mb-2 px-4"
-        initial="hidden"
-        animate="visible"
-        transition={{ duration: 1 }}
-        variants={titleVariants}
-        >¡Promociona tu Negocio!</motion.h2>
-        <motion.p className="  text-md  xl:text-lg  text-white text-center px-4 mb-2"
-         initial="hidden"
-         animate="visible"
-         transition={{ duration: 1, delay: 0.5 }}
-         variants={paragraphVariants}
+      <div className="absolute inset-0 flex flex-col justify-center items-center bg-black bg-opacity-30">
+        <motion.h2 className="text-2xl md:text-3xl font-bold text-white text-center mb-2 px-4"
+          initial="hidden"
+          animate="visible"
+          transition={{ duration: 1 }}
+          variants={titleVariants}
+        >
+          ¡Promociona tu Negocio!
+        </motion.h2>
+        <motion.p className="text-lg text-white text-center px-4 mb-2"
+          initial="hidden"
+          animate="visible"
+          transition={{ duration: 1, delay: 0.5 }}
+          variants={paragraphVariants}
         >
           ¿Quieres publicar o ver nuestras ofertas para promocionar tu negocio?
         </motion.p>
 
-             <Link to="/services" className='hover:text-[#ACE2E1] text-white px-1 py-2 block md:inline-block transform transition-transform duration-300 hover:scale-105'>
-           
-        <motion.button className="bg-[#41C9E2] text-black py-2 px-4 rounded hover:bg-[#008DDA] "
-         initial="hidden"
-         animate="visible"
-         transition={{ duration: 0.5, ease: 'easeInOut' }}
-         variants={buttonVariants}
-         >
-      Ver Ofertas
-        </motion.button>
-          </Link>
+        <Link to="/services" className='hover:text-[#ACE2E1] text-white px-1 py-2 block md:inline-block transform transition-transform duration-300 hover:scale-105'>
+          <motion.button className="bg-[#008DDA] text-white py-2 px-4 rounded hover:bg-[#41C9E2]"
+            initial="hidden"
+            animate="visible"
+            transition={{ duration: 0.5, ease: 'easeInOut' }}
+            variants={buttonVariants}
+          >
+            Ver Ofertas
+          </motion.button>
+        </Link>
       </div>
     </motion.div>
   );
